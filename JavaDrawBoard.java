0640bee05d5dd23a5a5993d9bfb97c307e7071fb
Ramda Yanurzha <ramda.yz@gmail.com>
2012-01-10 13:45:22 +0800

    January 10th. Still not finished.

--- JavaDrawBoard.java
+++ JavaDrawBoard.java
@@ -5,15 +5,20 @@
 package assignmenttwo;
 
 import java.awt.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseAdapter;
 import java.awt.image.BufferedImage;
 import java.math.*;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import javax.imageio.ImageIO;
 
 
@@ -21,6 +26,8 @@ public class JavaDrawBoard implements ActionListener{
     JFrame frame;
     DrawBoard mousePad;
     JLabel jl;
+    BufferedImage bimaster;
+    int mode;
     
     int x = 0;
     int y = 0;
@@ -37,6 +44,7 @@ public class JavaDrawBoard implements ActionListener{
         mousePad = new DrawBoard();
         frame.getContentPane().add(mousePad, BorderLayout.CENTER);
         mousePad.addMouseMotionListener(new mouseListener());
+        mousePad.addMouseListener(new mouseListener2());
         //jl = new JLabel("Click and Drag");
         //frame.getContentPane().add(jl, BorderLayout.SOUTH);
         JMenuBar mainmenu = menu();
@@ -44,6 +52,7 @@ public class JavaDrawBoard implements ActionListener{
         frame.repaint();
         frame.setSize(800, 800);
         frame.setVisible(true);
+        mode = 1;   //1 for square, 2 for circle, 3 for triangle
       }
 
     @Override
@@ -53,12 +62,19 @@ public class JavaDrawBoard implements ActionListener{
             System.exit(0);
         } else if ("save".equals(e.getActionCommand())){
             System.out.print("save");
-            mousePad.save("new.png");
-            //
+            save("save.png",bimaster);
         }
     }
-
-   class mouseListener extends MouseMotionAdapter {
+        
+        public void save(String imageFile, BufferedImage bi) {
+            try {
+                ImageIO.write(bi, "png", new File(imageFile));
+            } catch (IOException e) {
+            }
+        }
+        
+       
+    class mouseListener extends MouseMotionAdapter {
         @Override
         public void mouseDragged(MouseEvent e){
             //jl.setText("" + e.getX() + "," + e.getY());
@@ -68,6 +84,14 @@ public class JavaDrawBoard implements ActionListener{
         }
     }
     
+    class mouseListener2 extends MouseAdapter {
+        @Override
+        public void mouseReleased(MouseEvent e){
+            //jl.setText("" + e.getX() + "," + e.getY());
+             mode = getRandomInt(4);
+        }
+    }
+   
     public JMenuBar menu(){
         JMenuBar m = new JMenuBar();
         JMenu file = new JMenu("File");
@@ -91,46 +115,39 @@ public class JavaDrawBoard implements ActionListener{
         return m;
     }
     
-   
-    class DrawBoard extends JPanel {
-        
-        public void save(String imageFile) {
-            Rectangle r = getBounds();
-            try {
-                BufferedImage i = new BufferedImage(r.width, r.height,
-                    BufferedImage.TYPE_INT_ARGB);
-                Graphics g = i.getGraphics();
-                paint(g);
-                ImageIO.write(i, "png", new File(imageFile));
-            } catch (IOException e) {
-            }
-        }
+    public int getRandomInt(int factor){
+            int r = (int) (Math.random() * factor);
+            return r;
+    }
+    
+    
+    public class DrawBoard extends JPanel {
         
         
         @Override
-        public void paint(Graphics g) { //paintComponent()
-            //BufferedImage bi = new BufferedImage(DrawBoard.WIDTH, DrawBoard.HEIGHT, BufferedImage.TYPE_INT_ARGB);
-            //Graphics2D g2d = bi.createGraphics();
+        public void paint(Graphics g) { 
+            bimaster = new BufferedImage(800, 800,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2s = bimaster.createGraphics();
             Graphics2D g2d = (Graphics2D) g;
-            int r = (int) (Math.random() * 256);
-            int gr = (int) (Math.random() * 256);
-            int b = (int) (Math.random() * 256);
+            int r = getRandomInt(256);
+            int gr = getRandomInt(256);
+            int b = getRandomInt(256);
             g2d.setColor(new Color(r,gr,b));
-            int c = (int) (Math.random() * 4);
-            int scale = (int) (Math.random()  * 3);
-            double rotate = Math.random() * 360;
+            int c = getRandomInt(4);
+            int scale = getRandomInt(3);
             Point p1 = new Point(50 / 3, (2 * 50) / 3);
             Point p2 = new Point(50 / 2, 50 / 3);
             Point p3 = new Point((2 * 50) / 3, (2 * 50) / 3);
             int[] xs = { p1.x+x, p2.x+x, p3.x+x };
             int[] ys = { p1.y+y, p2.y+y, p3.y+y };
             Polygon tr = new Polygon(xs,ys,xs.length);
-            Rectangle rect = new Rectangle(x,y,25*scale,25*scale);
-            g2d.rotate(rotate,x,y);
-            switch (c) {
-                case 1: g2d.fillOval(x,y,25*scale,25*scale);break;
-                case 2: g2d.fill(rect);break;
-                case 3: g2d.fillPolygon(tr);break;
+            Rectangle rect = new Rectangle(x,y,25,25);
+            switch (mode) {
+                case 1: g2d.fillOval(x,y,25,25);g2s.fillOval(x, y, 25, 25);
+                    break;
+                case 2: g2d.fill(rect);g2s.fill(rect);break;
+                case 3: g2d.fillPolygon(tr);g2s.fillPolygon(tr);break;
                 default: break;
             }
             //g2d.drawImage(bi, null, 0, 0);
